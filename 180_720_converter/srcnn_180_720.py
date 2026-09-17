ef4c0e88956f445fba0b6466c0000c2ae95d6a08
Yadavendhiran <oddashettyyadu@gmail.com>
2026-09-17 14:10:51 +0530

    Current version V1 need changes

--- 180_720_converter/srcnn_180_720.py
+++ 180_720_converter/srcnn_180_720.py
@@ -5,24 +5,28 @@ class SRCNN(nn.Module):
         super().__init__()
         self.encoder=nn.Sequential(
             # layer 1
-            nn.Conv2d(in_channels=3,out_channels=8,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(in_channels=3,out_channels=16,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
 
             # layer 2
-            nn.Conv2d(in_channels=8,out_channels=12,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(in_channels=16,out_channels=32,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
 
             # layer 3
-            nn.Conv2d(in_channels=12,out_channels=12,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
 
+            # trial layer
+            nn.Conv2d(in_channels=64,out_channels=64,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            
             # layer 4
-            nn.Conv2d(in_channels=12,out_channels=8,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(in_channels=64,out_channels=32,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
 
             nn.Upsample(size=(720,1280),mode="bilinear",align_corners=False),
             # layer 5
-            nn.Conv2d(in_channels=8,out_channels=3,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(in_channels=32,out_channels=3,kernel_size=3,stride=1,padding=1),
         )
 
     def forward(self,x):
