905e6701976074b1835047a2981d8330400ef43c
Yadavendhiran <oddashettyyadu@gmail.com>
2026-09-17 14:13:01 +0530

    Cehcking the model usecase

---
+++ 180_720_converter/video_no_check_180.py
@@ -0,0 +1,61 @@
+import cv2
+from torchvision import transforms
+import torch
+import numpy
+import time
+import torch.nn as nn
+from srcnn_180_720 import SRCNN
+
+
+video=cv2.VideoCapture(r"A:\rec\gif\peace sign.mp4")
+device="cpu"
+transform=transforms.ToTensor()
+initial_total_time=time.perf_counter() # total time
+model=SRCNN()
+model.load_state_dict(torch.load("SRCNN_1.5X_v0.pth",map_location="cpu"))    
+model.eval()
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fps=video.get(cv2.CAP_PROP_FPS)
+video_writer=cv2.VideoWriter("output_720_9.mp4",fps=fps,frameSize=(1280,720),fourcc=fourcc)
+model_time=0
+tranform_time=0
+write_time=0
+frame_count=0
+while True:
+    success,frame=video.read()
+
+    if not success:
+            break
+    
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)  # CV capture frame -> Tensor
+    tensor_frame=transform(frame)
+    tensor_frame=tensor_frame.unsqueeze(0)
+    frame_count+=1
+
+    intital_model_time=time.perf_counter() # CV tensor to upscale model
+    upscale=model(tensor_frame)
+    final_model_time=time.perf_counter()
+    model_time+=(final_model_time-intital_model_time)
+
+    initial_transform_time=time.perf_counter() # Upscaled tensor -> image
+    upscale=upscale.squeeze(0)
+    upscale=upscale.permute(1,2,0)
+    upscale=upscale.detach().numpy()
+    upscale=(upscale*255).clip(0,255).astype(numpy.uint8)
+    upscale=cv2.cvtColor(upscale,cv2.COLOR_RGB2BGR)
+    final_transform_time=time.perf_counter()
+    tranform_time+=(final_transform_time-initial_transform_time)
+
+    initial_video_time=time.perf_counter() # iamge/frame -> video *mp4v
+    video_writer.write(upscale)
+    final_video_time=time.perf_counter()
+    write_time+=final_video_time-initial_video_time
+
+video_writer.release()
+video.release()
+final_total_time=time.perf_counter()
+print("FInal time:",((final_total_time-initial_total_time)))
+print("total model Time:",model_time)
+print("total transform Time:",tranform_time)
+print("total write time:",write_time)
+print("frame:",frame_count)
